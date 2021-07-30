6517b7cbad27bea7eb1e7426ec387b02d35c7505
ambalikajaiswal <66306016+ambalikajaiswal@users.noreply.github.com>
2021-07-30 13:13:54 +0530

    Create gcd of array.cpp

---
+++ company wise/sap labs/gcd of array.cpp
@@ -0,0 +1,27 @@
+/*Input: N = 3, arr[] = {2, 4, 6}
+Output: 2
+Explanation: GCD of 2,4,6 is 2.*/
+
+class Solution
+{
+	public:
+	int gc(int a,int b)
+{
+     if(b==0)
+     return a;
+     return gc(b,a%b);
+}
+
+int gcd(int n,int a[])
+{
+    int res=a[0];
+    for(int i=1;i<n;i++)
+    {
+        res=gc(res,a[i]);
+        
+        if(res==1)
+        return 1;
+    }
+    return res;
+}
+};
