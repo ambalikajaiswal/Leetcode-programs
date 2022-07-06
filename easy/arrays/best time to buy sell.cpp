38b739c35dcecf6acdde2f48cd85d49812f65f5e
ambalikajaiswal <66306016+ambalikajaiswal@users.noreply.github.com>
2022-07-06 21:08:11 +0530

    Create best time to buy sell.cpp

---
+++ easy/arrays/best time to buy sell.cpp
@@ -0,0 +1,18 @@
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price=INT_MAX;
+        int max_pro=0;
+        for(int i=0;i<prices.size();i++)
+        {
+            min_price=min(min_price,prices[i]);
+            max_pro=max(max_pro,prices[i]-min_price);
+        }
+        return max_pro;
+    }
+};
