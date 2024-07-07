7b500b064d0fd6e5550a33a773763aa58592d547
ambalikajaiswal <66306016+ambalikajaiswal@users.noreply.github.com>
2024-07-07 14:16:56 +0530

    Update two sum.cpp

--- Arrays/two sum.cpp
+++ Arrays/two sum.cpp
@@ -23,3 +23,22 @@ public:
 };
 
 //give the indices of the pair sum and above codde will only work if sorted
+
+O[n] solution using unordered map so that sorting is not required
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& a, int target) {
+        unordered_map<int,int> vis;
+
+        int n =a.size();
+        for(int i=0;i<=n;++i){
+            int b = target-a[i]; // this is element that we need to find in the map 
+            auto it= vis.find(b); //it is an iterator used to find the element in map,check if element foundin map
+            if(it!=vis.end()){ // if b is not found in map ,it=vi.end()->returns an iterator pointing to the position just beyond the last element in 
+                return{it->second,i};//if found then return indices of two numbers
+            }
+            vis[a[i]]=i;//   If element is not found, add the current number to the map
+        }
+        return{-1,-1};
+    }
+};
