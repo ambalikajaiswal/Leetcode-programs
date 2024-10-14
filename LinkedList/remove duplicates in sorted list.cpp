b95378426d49599002639e96ee0cd5ba9496beab
ambalikajaiswal <66306016+ambalikajaiswal@users.noreply.github.com>
2024-10-14 10:21:43 -0700

    Update remove duplicates in sorted list.cpp

--- LinkedList/remove duplicates in sorted list.cpp
+++ LinkedList/remove duplicates in sorted list.cpp
@@ -25,3 +25,19 @@ public:
         return head;
     }
 };
+
+
+python
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        temp=head
+        if head is None:
+            return None
+        while temp.next:
+            if temp.val==temp.next.val:
+                temp.next=temp.next.next
+            else:
+                temp=temp.next
+        return head
+        
