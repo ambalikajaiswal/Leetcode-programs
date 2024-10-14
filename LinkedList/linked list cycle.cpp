1066113304d7513930bae68929e3dc1587de0fab
ambalikajaiswal <66306016+ambalikajaiswal@users.noreply.github.com>
2024-10-14 10:40:53 -0700

    Update linked list cycle.cpp

--- LinkedList/linked list cycle.cpp
+++ LinkedList/linked list cycle.cpp
@@ -27,3 +27,14 @@ public:
         return NULL;
     }
 };
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow=head
+        fast=head
+        while slow and fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
+                return True
+        return False
