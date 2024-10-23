ad21c2cf7d274de0b018bbdd10cab3e6ad13a398
ambalikajaiswal <66306016+ambalikajaiswal@users.noreply.github.com>
2024-10-23 11:24:17 -0700

    Update remove nth node from list.cpp

--- LinkedList/remove nth node from list.cpp
+++ LinkedList/remove nth node from list.cpp
@@ -26,3 +26,51 @@ public:
         return dummy->next;
     }
 };
+
+
+class Solution {
+public:
+    int count(ListNode* head){
+        ListNode* temp=head;
+        int cnt=0;
+        while(temp!=NULL){
+            cnt++;
+            temp=temp->next;
+        }
+        return cnt;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* temp=head;
+        if(head==NULL)
+        return NULL;
+        int cnt=count(head);
+        if(cnt==n){
+            return head->next;
+        }
+        int x=cnt-n;
+        for(int i=1;i<x;i++){
+            temp=temp->next;
+        }
+        ListNode*p=temp->next;
+        temp->next=p->next;
+
+
+//python
+        class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if n==0:
+            return None
+        fast=slow=head
+        for i in range(n):
+            fast=fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            slow=slow.next
+            fast=fast.next
+        slow.next=slow.next.next
+        return head
+        delete p;
+        return head;
+    }
+};
