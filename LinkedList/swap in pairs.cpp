d49739d6dae11ea72ba133e30db8dcb7938b018c
ambalikajaiswal <66306016+ambalikajaiswal@users.noreply.github.com>
2024-10-24 09:54:46 -0700

    Update swap in pairs.cpp

--- LinkedList/swap in pairs.cpp
+++ LinkedList/swap in pairs.cpp
@@ -16,3 +16,15 @@ public:
         return head;
     }
 };
+
+//python
+class Solution:
+    def swap(a,b):
+        a,b=b,a
+
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp=head
+        while temp is not None and temp.next is not None:
+            temp.val,temp.next.val=temp.next.val,temp.val
+            temp=temp.next.next
+        return head
