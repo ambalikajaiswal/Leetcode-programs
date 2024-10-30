9c54a64bef8f3ad3791e1994dca1a6c1f3397397
ambalikajaiswal <66306016+ambalikajaiswal@users.noreply.github.com>
2024-10-30 10:05:03 -0700

    Update rotate the list.cpp

--- LinkedList/rotate the list.cpp
+++ LinkedList/rotate the list.cpp
@@ -1,5 +1,70 @@
 class Solution {
 public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL)
+         return NULL;
+        vector<int>v;
+        ListNode*temp=head;
+        while(temp!=NULL){
+            v.push_back(temp->val);
+            temp=temp->next;
+        }
+        int n=v.size();
+        k=k%n;
+        reverse(v.begin(),v.end());
+        reverse(v.begin(),v.begin()+k);
+        reverse(v.begin()+k,v.end());
+        
+        temp=head;
+        for(int i=0;i<v.size();i++){
+            temp->val=v[i];
+            temp=temp->next;
+        }
+        return head;
+    }
+};
+
+//approach 2
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL||head->next==NULL||k==0)
+          return head;
+        int cnt=1;
+        ListNode*tail=head;
+        while(tail->next!=NULL){
+            tail=tail->next;
+            cnt++;
+        }
+        if(k>cnt){
+            k=k%cnt;
+            if(k==0)
+             return head;
+        }
+
+        if(k<=cnt){
+            k=cnt-k;
+            if(k==0){
+                return head;
+            }
+        }
+        int i=1;
+        ListNode* curr=head;
+        while(i<k && curr!=NULL){
+            curr=curr->next;
+            i++;
+        }
+
+        ListNode*temp=curr->next;
+        curr->next=NULL;
+        tail->next=head;
+        return temp;
+    }
+};
+
+
+class Solution {
+public:
     int get(ListNode*head)
     {  int cnt=0;
         ListNode*temp=head;
